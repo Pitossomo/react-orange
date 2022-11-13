b2ebe1387218a6d349aa321fae2359a791ca547f
pitossomo <pedro.h.teles@hotmail.com>
2022-11-13 07:51:02 -0300

    Calculadora grid structure added

--- pages/calculadora.tsx
+++ pages/calculadora.tsx
@@ -1,3 +1,24 @@
+import styles from "../styles/Calculadora.module.css";
+
 export default function Calculadora() {
-  return <h1>Calculadora</h1>;
+  const buttonLabels = [
+    "x²", "√", "C", "/",
+    7, 8, 9, "x",
+    4, 5, 6, "-",
+    1, 2, 3, "+",
+    "±", 0, ",", "=" 
+  ];
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.calculator}>
+        <div className={styles.display}></div>
+        {
+          buttonLabels.map(label => (
+            <button>{label}</button>
+          ))
+        }
+      </div>
+    </div>
+  )
 }

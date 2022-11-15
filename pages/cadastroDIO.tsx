7e0931b9c1a968f66a641142806be3c02101cc72
pitossomo <pedro.h.teles@hotmail.com>
2022-11-15 08:07:40 -0300

    style added to DIO's clone

--- pages/cadastroDIO.tsx
+++ pages/cadastroDIO.tsx
@@ -35,21 +35,23 @@ export default function CadastroDIO() {
             </div>
             <div className={styles.inputWrapper}>
               <svg className={styles.icon} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"   height="1em" width="1em">
-                <path fill="none" d="M0 0h24v24H0z"></path><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"></path>
+                <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"></path>
               </svg>
               <input className={styles.input} placeholder="E-mail"></input>
             </div>
             <div className={styles.inputWrapper}>
               <svg className={styles.icon} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"   height="1em" width="1em">
-                <g><path fill="none" d="M0 0h24v24H0z"></path><path d="M18 8h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h2V7a6 6 0 1 1 12 0v1zm-2 0V7a4 4 0 1 0-8 0v1h8zm-5 6v2h2v-2h-2zm-4 0v2h2v-2H7zm8 0v2h2v-2h-2z"></path></g>
+                <path d="M18 8h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h2V7a6 6 0 1 1 12 0v1zm-2 0V7a4 4 0 1 0-8 0v1h8zm-5 6v2h2v-2h-2zm-4 0v2h2v-2H7zm8 0v2h2v-2h-2z"></path>
               </svg>
               <input className={styles.input} placeholder="Password"></input>
             </div>
-            <button>Criar minha conta</button>
+            <button className={styles.formButton}>Criar minha conta</button>
             <p>
               Ao clicar em "criar minha conta grátis", declaro que aceito as Políticas de Privacidade e os Termos de Uso da DIO.
             </p>
-            <strong>Já tenho conta. <a>Fazer login</a></strong>
+            <strong className={styles.strong}>
+              Já tenho conta. <a href="#" className={styles.greenText}>Fazer login</a>
+            </strong>
           </form>
         </div>
       </main>

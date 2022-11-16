43d49a8b20819ac21e856570f380e7d8754d7470
pitossomo <pedro.h.teles@hotmail.com>
2022-11-16 06:03:58 -0300

    react-hook-forms imported

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,32 @@
 
+import { useForm } from "react-hook-form"
 import styles from "../styles/Login.module.scss"
 
 export default function Login () {
+  const { 
+    register,
+    watch,
+    formState: {errors, isValid },
+  } = useForm()
+
+  const form = watch()
+  console.log(form)
 
   return (
     <div className={styles.container}>
       <form className={styles.form}>
         <h1 className={styles.title}>Login</h1>
-        <input className={styles.input} placeholder="Usuário" />
-        <input className={styles.input} placeholder="Senha" type="password" />
+        <input 
+          className={styles.input}
+          placeholder="Usuário"
+          {...register("email")}
+        />
+        <input
+          className={styles.input}
+          placeholder="Senha"
+          type="password"
+          {...register("password")}
+        />
         <button className={styles.button}>Entrar</button>
       </form>
     </div>
